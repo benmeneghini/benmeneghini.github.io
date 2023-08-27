e035fa7d27727bfa6ac9665e689628b86d372937
Ben Meneghini <bme42@uclive.ac.nz>
2023-08-27 20:52:07 +1200

    Added curved line graphic and sectioned out hero page to be under main section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -40,7 +40,45 @@ const Home = () => {
     }
 
     return (
-        <div className="bg-red-50 h-screen font-mono">
+        <div className="h-screen font-mono">
+            <div id="top" className="h-full">
+                <div className="flex flex-col fixed z-[-30] md:ml-40 px-12 text-5xl md:text-8xl text-start h-48 md:h-72 place-content-center">
+                    <p>Ben</p>
+                    <p>Meneghini.</p>
+                </div>
+                <svg className="invisible md:visible h-full fixed z-[-50]" width="1440" height="560" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlnsXlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none" viewBox="0 0 1440 560">
+                    <g mask="url(&quot;#SvgjsMask1002&quot;)" fill="none">
+                        <rect width="1440" height="560" x="0" y="0" fill="rgba(254, 242, 242, 1)"></rect>
+                        <path d="M685.65 598.61C785.27 578.96 731.99 338.21 985.15 324.27 1238.3 310.33 1426.6 130.74 1584.14 128.27" stroke="rgba(69, 10, 10, 0.58)" stroke-width="2"></path>
+                        <path d="M652.58 594.76C751.45 562.98 688.19 293.55 919.82 276.72 1151.44 259.89 1315.44 126.97 1454.29 125.52" stroke="rgba(69, 10, 10, 0.58)" stroke-width="2"></path>
+                        <path d="M793.37 589.49C918.79 523.45 854.71 111.02 1090.66 106.68 1326.61 102.34 1530.4 278.52 1685.24 280.28" stroke="rgba(69, 10, 10, 0.58)" stroke-width="2"></path>
+                        <path d="M492.27 582.84C604.12 559.12 547.08 280.78 823.84 267.83 1100.6 254.88 1315.62 95.66 1486.99 94.23" stroke="rgba(69, 10, 10, 0.58)" stroke-width="2"></path>
+                        <path d="M378.35 667.71C482.51 661.62 450.98 477.59 746.18 470.51 1041.39 463.43 1289.61 248.97 1481.85 246.51" stroke="rgba(69, 10, 10, 0.58)" stroke-width="2"></path>
+                    </g>
+                    <defs>
+                        <mask id="SvgjsMask1002">
+                            <rect width="1440" height="560" fill="#ffffff"></rect>
+                        </mask>
+                    </defs>
+                </svg>
+
+                <svg className="visible md:invisible h-full fixed z-[-50] bg-red-50" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlnsXlink="http://www.w3.org/1999/xlink" width="1440" height="560" preserveAspectRatio="none" viewBox="0 0 1440 560">
+                    <g mask="url(&quot;#SvgjsMask1007&quot;)" fill="none">
+                        <rect width="1440" height="560" x="0" y="0" fill="rgba(254, 242, 242, 1)"></rect>
+                        <path d="M921.78-45.65C806.64-18.2 862.28 280.6 588.58 290.71 314.87 300.82 91.11 402.32-77.83 402.71" stroke="rgba(69, 10, 10, 0.58)" stroke-width="2"></path>
+                        <path d="M847.93-91.55C723.09-27.84 788.84 377.33 549.43 382.99 310.01 388.65 105.4 249.42-47.58 248.59" stroke="rgba(69, 10, 10, 0.58)" stroke-width="2"></path>
+                        <path d="M803.67-80.46C667.75-72.77 564.71 174 322.35 174.2 79.99 174.4-33.66 35.61-158.97 34.2" stroke="rgba(69, 10, 10, 0.58)" stroke-width="2"></path>
+                        <path d="M1284.98-24.78C1097.48-20.02 959.67 238.97 585.99 247.77 212.3 256.57 73.18 501.31-113.01 505.37" stroke="rgba(69, 10, 10, 0.58)" stroke-width="2"></path>
+                        <path d="M697.15-89.61C582.74-56.13 651.4 250.13 380.85 267.23 110.3 284.33-88.12 433.81-251.75 435.23" stroke="rgba(69, 10, 10, 0.58)" stroke-width="2"></path>
+                    </g>
+                    <defs>
+                        <mask id="SvgjsMask1007">
+                            <rect width="1440" height="560" fill="#ffffff"></rect>
+                        </mask>
+                    </defs>
+                </svg>
+            </div>
+
             <div id="navbar" className="invisible md:visible">
                 <Navbar />
             </div>
@@ -64,11 +102,7 @@ const Home = () => {
                 </button>
             </div>
 
-            <div id="main" className="bg-red-50 md:ml-40 px-12 py-5">
-                <div className="flex flex-col text-5xl md:text-8xl text-start h-48 md:h-72 place-content-center">
-                    <p>Ben</p>
-                    <p>Meneghini.</p>
-                </div>
+            <div id="main" className="bg-red-50 md:ml-40 px-12 py-5 z-50">
 
                 <br/>
                 <hr/>
@@ -170,10 +204,12 @@ const Home = () => {
                         <span className="ml-2">{Info.number}</span>
                     </div>
 
-                    <br/>
+                    {/*<div className="md:col-start-3 bg-neutral-600">*/}
+                    {/*    Test*/}
+                    {/*</div>*/}
 
                     <div className="md:col-start-2 mb-2">
-                        <button className="border-2 rounded shadow p-2 mt-2"><Link className="flex items-center hover:cursor-pointer" to="main" spy={true} smooth={true} duration={500}>Back to Top<svg xmlns="https://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-2">
+                        <button className="border-2 rounded shadow p-2 mt-2"><Link className="flex items-center hover:cursor-pointer" to="top" spy={true} smooth={true} duration={500}>Back to Top<svg xmlns="https://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-2">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M9 9l6-6m0 0l6 6m-6-6v12a6 6 0 01-12 0v-3" />
                         </svg></Link>
                         </button>
